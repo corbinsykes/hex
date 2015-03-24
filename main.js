06f0256670d56d515ef727eb263800abf00ee343
Corbin Sykes <corbin.sykes@gmail.comm>
2015-03-24 13:47:02 -0400

    comment removal

--- main.js
+++ main.js
@@ -3,7 +3,6 @@ window.onload = function() {
 	function updateTime() {
 		var currentDate = new Date();
 
-
 				currentHour = ('0' + currentDate.getHours()).slice(-2);
 				currentMinute = ('0' + currentDate.getMinutes()).slice(-2);
 				currentSecond = ('0' + currentDate.getSeconds()).slice(-2);
@@ -13,20 +12,6 @@ window.onload = function() {
 				currentMinuteHex = Math.round(currentMinute*4.267).toString(16);
 				currentSecondHex = Math.round(currentSecond*4.267).toString(16);
 				currentTimeHex = currentHourHex + currentMinuteHex + currentSecondHex;
-
-				londonHour =  parseInt(currentHour) + 4;
-				if (londonHour == "24") {
-					londonHour = "00";
-				}
-
-				if (londonHour > "24") {
-					londonHour = parseInt(londonHour) - 24;
-				}
-
-				londonHourHex = Math.round(londonHour*10.67).toString(16);
-				londonTime = londonHour + ":" + currentMinute + ":" + currentSecond;
-				londonTimeHex = londonHourHex + currentMinuteHex + currentSecondHex;
-
 
 				if (currentHourHex == "0") {
 					currentHourHex = "00";
@@ -38,18 +23,10 @@ window.onload = function() {
 					currentSecondHex = "00";
 				}
 
-
-
-				console.log(londonHour);
-
 		document.getElementById('current-time').innerHTML = currentTime;
 		document.getElementById('current-hex').innerHTML = "#" + currentTimeHex;
 
-		document.getElementById('london-time').innerHTML = londonTime;
-		document.getElementById('london-hex').innerHTML = "#" + londonTimeHex;
-
-		document.getElementById('current-time').style.background = "#" + currentTimeHex;
-		document.getElementById('london-time').style.background = "#" + londonTimeHex;
+		document.getElementById('body').style.background = "#" + currentTimeHex;
 	}
 
 	setInterval(updateTime, 1000);
